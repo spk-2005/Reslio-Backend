02c76819c7c05f1df1a0f4de6c3043c03e27eee1
spk-2005 <prasannasimha5002@gmail.com>
2025-10-28 17:53:02 +0530

    added template creation route

--- routes/templates.js
+++ routes/templates.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const { getAllTemplates, getTemplateById, createTemplate } = require('../controllers/templateController');
-const { authenticateUser } = require('../middleware/auth');
+const { authenticateAdmin } = require('../middleware/adminAuth'); // Import the correct admin middleware
 
+// Public routes to allow the mobile app to see templates
 router.get('/', getAllTemplates);
 router.get('/:id', getTemplateById);
-router.post('/', authenticateUser, createTemplate);
+
+// Admin-only route to create templates
+router.post('/', authenticateAdmin, createTemplate);
 
 module.exports = router;
+
