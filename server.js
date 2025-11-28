ac6220f6213a363b09c01c7db225580e01759db7
spk-2005 <prasannasimha5002@gmail.com>
2025-11-28 07:10:40 +0530

    first commit

--- server.js
+++ server.js
@@ -17,7 +17,11 @@ const app = express();
 
 connectDB();
 
-app.use(cors());
+app.use(cors({
+  origin: '*', // Allow all origins
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
